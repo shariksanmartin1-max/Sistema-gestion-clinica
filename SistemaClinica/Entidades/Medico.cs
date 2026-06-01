e8899ad028716d6dd6b33e1df8e8dfe1e1b198d8
Sharik Jusmary Sanmartin Pineda <ssanmarti8@utmachala.edu.ec>
2026-05-31 21:16:38 -0500

    agg medico

---
+++ SistemaClinica/Entidades/Medico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaClinica.Entidades
+{
+    public class Medico
+    {
+        public string Codigo { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Especialidad { get; set; }
+        public string Telefono { get; set; }
+    }
+}
