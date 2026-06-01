a5209fa2d718e3084285fd8b59d6a99592ee7d29
Sharik Jusmary Sanmartin Pineda <ssanmarti8@utmachala.edu.ec>
2026-05-31 22:55:51 -0500

    Agg carpeta Formularios

--- SistemaClinica/Formularios/FrmMenu.Designer.cs
+++ SistemaClinica/Formularios/FrmMenu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SistemaClinica
 {
-    partial class Form1
+    partial class FrmMenu
     {
         /// <summary>
         /// Variable del diseñador necesaria.
--- SistemaClinica/Formularios/FrmMenu.cs
+++ SistemaClinica/Formularios/FrmMenu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SistemaClinica
 {
-    public partial class Form1 : Form
+    public partial class FrmMenu : Form
     {
-        public Form1()
+        public FrmMenu()
         {
             InitializeComponent();
         }
