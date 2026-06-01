bee011be33f523867d5e8bc86ea940dda4cba68a
Sharik Jusmary Sanmartin Pineda <ssanmarti8@utmachala.edu.ec>
2026-05-31 23:12:25 -0500

    Agg los formularios necesarios

---
+++ SistemaClinica/Formularios/FrmCitas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaClinica.Formularios
+{
+    public partial class FrmCitas : Form
+    {
+        public FrmCitas()
+        {
+            InitializeComponent();
+        }
+    }
+}
